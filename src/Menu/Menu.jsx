d3a50aefc7183729b13e8945c605ee40c85c1b1a
Tania Ali <taniafali@gmail.com>
2024-02-02 22:54:18 -0800

    Refactor to improve accessibility

--- src/Menu/Menu.jsx
+++ src/Menu/Menu.jsx
@@ -6,14 +6,21 @@ const MenuContext = React.createContext()
 
 export default function Menu({ children }) {
     const [open, setOpen] = React.useState(false)
-
+    const menuId = React.useId();
     function toggle() {
         setOpen(prevOpen => !prevOpen)
     }
+    // Improve accessibility by:
+    /* apply role = "menu" 
+       aria-expanded on dropdown
+       aria-haspopup
+       aria-hidden
+       aria-controls on button and button-action component i.e dropdown
 
+    */
     return (
-        <MenuContext.Provider value={{open, toggle}}>
-            <div className="menu">
+        <MenuContext.Provider value={{open, toggle, menuId}}>
+            <div className="menu" role="menu">
                 {children}
             </div>
         </MenuContext.Provider>
