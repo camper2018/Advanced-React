122ff8c506b874d7ddca938ecc9d83a64368aaa8
Tania Ali <taniafali@gmail.com>
2024-05-19 16:31:13 -0700

    Passing onToggle event listener to headless Toggle component

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,16 @@
-import React from 'react';
+import React, {useState} from 'react';
 import ReactDOM from 'react-dom/client';
 import Menu from "./Menu/index"
 import { BsStar, BsStarFill } from 'react-icons/bs';
 import Star from "./Star"
 function App() {
   const sports = ["Tennis", "Pickle ball", "Racquet ball", "Squash"]
-
+  const [count, setCount] = useState(0);
+  console.log("# of times star was clicked: ", count)
   return (
     <>
-      <Star/>
-      <br />
+      <Star onChange ={() => setCount(prev => prev + 1)}/>
+      {/* <br />
         <Menu>
           <Menu.Button>Sports</Menu.Button>
             <Menu.Dropdown>
@@ -17,7 +18,7 @@ function App() {
                 <Menu.Item key={sport}>{sport}</Menu.Item>
               ))}
             </Menu.Dropdown>
-        </Menu>
+        </Menu> */}
     </>
   )
 }
--- src/Toggle/Toggle.jsx
+++ src/Toggle/Toggle.jsx
@@ -1,14 +1,16 @@
-import React from "react"
+import React, {useEffect} from "react"
 
 const ToggleContext = React.createContext()
 
-export default function Toggle({ children }) {
+export default function Toggle({ children, onToggle }) {
     const [on, setOn] = React.useState(false)
-
+   
     function toggle() {
         setOn(prevOn => !prevOn)
     }
-
+    useEffect(() => {
+       onToggle()
+    }, [on])
     return (
         <ToggleContext.Provider value={{ on, toggle }}>
             {children}
